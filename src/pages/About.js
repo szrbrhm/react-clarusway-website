cba9a74623406826faec86dbcdf59d57f945ef80
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-15 13:53:26 +0200

    add about pages

---
+++ src/pages/About.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <main className="main-bar main">
+      <div id="about">
+        <h3>About Us</h3>
+        <br />
+        <p id="about1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          odio numquam quisquam accusantium quos asperiores impedit at, sed quod
+          voluptatibus suscipit dolore. Vitae minima esse quia consequatur
+          dolor, odio quas deserunt non excepturi obcaecati atque aut.
+          Consequuntur, facere ullam? Esse distinctio sapiente modi alias
+          tempore assumenda quam ratione consequuntur quidem.
+        </p>
+        <br />
+        <br />
+        <div className="ali">
+          <p id="aliquam">
+            Aliquam eget pharetra diam. Nulla placerat lorem at turpis tempor,
+            vel ultrices dui tincidunt. Proin quis egestas lorem. Mauris
+            vehicula lectus odio, sit amet dictum justo feugiat a. Praesent id
+            dictum lacus. Sed ullamcorper id erat ut dictum. Fusce porttitor
+            lorem sapien, in aliquet sapien convallis et. Donec nec mauris
+            nulla. Curabitur cursus semper odio, et hendrerit ante. Nunc at
+            cursus ante. Maecenas gravida ligula ut efficitur suscipit. Nulla id
+            turpis varius, pretium nunc sed, fermentum sem. Sed lacinia nunc non
+            interdum pellentesque.
+          </p>
+        </div>
+      </div>
+
+      <div className="we-do">
+        <h4>What We Do</h4>
+        <p id="aliquam1">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio
+          non officiis corporis placeat quod inventore velit qui rem at quaerat
+          ullam necessitatibus voluptate eius doloremque, consequatur eaque
+          quidem omnis similique.
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default About;
