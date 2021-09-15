e27a8cfd26d54b344e72ecf819cb7b0753651e2d
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-15 13:09:09 +0200

    create subcrise comp

---
+++ src/components/Subcrise.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Subcrise = () => {
+    return (
+        <div>
+            <div id="class-nav">
+        <div className="subscribe">
+            <h1>Subscribe To Our Newsletter</h1>
+        </div>
+        <div className="input">
+            <input id="email" type="email" placeholder="Enter Email..."/>
+            <a id="subscrb" href="">Subsscribe</a>
+
+        </div>
+        </div>
+        </div>
+    )
+}
+
+export default Subcrise
